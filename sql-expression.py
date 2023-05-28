03b9e516a21bd06dc267174dc8f02c9a76a9f21c
Paul-Gleeson <Shagamatula@gmail.com>
2023-05-28 18:43:01 +0000

    working now

--- sql-expression.py
+++ sql-expression.py
@@ -10,8 +10,33 @@ meta = MetaData(db)
 # create a Var for "Artist" table
 artist_table = Table(
     "Artist", meta,
+    Column("ArtistId", Integer, primary_key=(True)),
+    Column("Name", String)
 )
 
+album_table = Table(
+    "Album", meta,
+    Column("AlbumId", Integer, primary_key=(True)),
+    Column("Title", String),
+    Column("ArtistId", Integer, ForeignKey(artist_table.ArtistId))
+)
+
+track_table = Table(
+    "Track", meta,
+    Column("TrackId", Integer, primary_key=(True)),
+    Column("Name", String),
+    Column("AlbumId", Integer, ForeignKey(album_table.TrackId)),
+    Column("MediaTypeId", Integer, primary_key=(False)),
+    Column("GenreId", Integer, primary_key=(False)),
+    Column("Composer", String),
+    Column("Milliseconds", Int),
+    Column("Bytes", String),
+    Column("Unit Price", Float),
+
+    
+    
+
+)
 
 # make connection
 with db.connect() as connection:

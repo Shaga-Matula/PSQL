c2c2e441a00bcb20076f21d91c26f5ed9a11d372
Paul-Gleeson <Shagamatula@gmail.com>
2023-05-29 16:48:59 +0000

    made me user

--- sql-crud.py
+++ sql-crud.py
@@ -100,6 +100,35 @@ paul_gleeson = Programmer(
 # session.commit()
 
 
+# programmer = session.query(Programmer).filter_by(id=7).first()
+# programmer.famous_for="World Presadent"
+
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender Not Provided")
+#     session.commit()
+
+# delete single record
+
+fname = input("Enter First Name: ")
+lname = input("Enter Last Name: ")
+programmer = session.query(Programmer).filter_by(first_name = fname, last_name=lname).first()
+if programmer is not None:
+    print("Programmer Found : ", programmer.first_name +
+          " " + programmer.last_name)
+    confirmation = input("Are you sure you want to delete this record (y/n) ")
+    if confirmation.lower() == "y":
+        session.delete(programmer)
+        session.commit()
+    print("Programmer has been deleted")
+else:
+    print("No Record found")
+
 # query the database to find all Programmers
 programmers = session.query(Programmer)
 for programmer in programmers:
